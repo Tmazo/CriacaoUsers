110eda8ea295e1a6824fc5e3524071c09a312b6e
Thiago Mazo <thiago.mazo@hotmail.com>
2020-05-08 18:55:35 -0300

    first commit

---
+++ Repository/Interfaces/IUsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Interfaces
+{
+    interface IUsersRepository
+    {
+        string GetAll();
+    }
+}
